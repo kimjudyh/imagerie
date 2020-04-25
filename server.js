d55c1dae732352566d9025e487d78e5d63876664
Judy <kimjudyh@gmail.com>
2020-04-25 15:11:38 -0700

    set up all middleware, installed express-session

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 // ======= IMPORTS
 const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const path = require('path');
+const session = require('express-session');
+
 const app = express();
 const port = process.env.PORT || 4000;
 
@@ -10,6 +15,15 @@ app.set('view engine', 'ejs');
 const authController = require('./controllers/authController');
 
 // ======= MIDDLEWARE
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+// TODO sett up session
+app.use(session({
+  secret: 'xo4SO*U#;ljxliha987DKJEI',
+  resave: false, // only save session if set or mutate property on session object
+  saveUninitialized: false, // only save a cookie when we set a property
+}));
 
 // ======= ROUTES
 app.get('/', (req, res) => {
