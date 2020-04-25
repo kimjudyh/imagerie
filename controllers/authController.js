3151bacf603d73da26950ccbab8f07bdaa38912d
Judy <kimjudyh@gmail.com>
2020-04-25 13:46:22 -0700

    set up barebones authController

---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+// ======== IMPORTS
+const express = require('express');
+const router = express.Router();
+
+// ======== MODELS
+const db = require('../models');
+
+// ======== ROUTES
+
+// ======== EXPORTS
+module.exports = router;
